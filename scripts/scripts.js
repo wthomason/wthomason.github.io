a3e3a9cfd9634adee67c46be08a24013f0404e08
Tyson Thomason <tthomason@mailserviceslc.com>
2019-03-14 10:30:12 -0500

    added content to index.html, added style to styel.css and created scripts/scripts.js

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+/*
+============================================
+; Title:  Portfolio Scripts
+; File Name: scripts.js
+; Author: William Thomason
+; Date:   14 March 2019
+; Description: Script file for portfolio site
+;===========================================
+*/
+
